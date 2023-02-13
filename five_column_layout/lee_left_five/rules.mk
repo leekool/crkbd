10c5ccff569f8919a0784c8889ead5616c60ff91
leekool <leejedda@gmail.com>
2023-02-13 18:26:24 +1100

    five column layout

---
+++ five_column_layout/lee_left_five/rules.mk
@@ -0,0 +1,12 @@
+MOUSEKEY_ENABLE = yes
+COMBO_ENABLE = yes
+
+# pimoroni uncommented on the right side
+POINTING_DEVICE_DRIVER = pimoroni_trackball
+
+PIMORONI_TRACKBALL_ENABLE = no
+# ifeq ($(strip $(PIMORONI_TRACKBALL_ENABLE)), yes)
+    # POINTING_DEVICE_ENABLE = yes
+    # SRC += drivers/sensors/pimoroni_trackball.c
+    # QUANTUM_LIB_SRC += i2c_master.c
+# endif
