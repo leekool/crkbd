0d87c276842ab2b5782cb07f246d9ad4fe044c4e
leekool <lee@imre.al>
2023-10-03 22:34:33 +1100

    tap times

--- keymap.c
+++ keymap.c
@@ -235,7 +235,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   case L1_BSPC:
     return TAPPING_TERM - 30;
   case L3_TAB:
-    return TAPPING_TERM - 65;
+    return TAPPING_TERM - 68;
   case Z_SFT:
     return TAPPING_TERM - 75;
   default:
