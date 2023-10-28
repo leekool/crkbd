8b3f3f5b5d121955834eaa162b42e49c00b52abd
leekool <lee@imre.al>
2023-10-28 16:14:24 +1100

    remove email

--- keymap.c
+++ keymap.c
@@ -109,7 +109,7 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
   switch(combo_index) {
   // case EM_EMAIL:
   //   if (pressed) {
-  //     SEND_STRING("lee@imre.al");
+  //     SEND_STRING("");
   //   }
   //   break;
   }
